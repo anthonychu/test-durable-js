e4146d6068ac8d6c8a1af209b192c1aeeda1cd8c
Anthony Chu <antchu@microsoft.com>
2019-11-16 22:51:01 -0800

    Initial commit

---
+++ DurableFunctionsOrchestratorJS/index.js
@@ -0,0 +1,7 @@
+﻿const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const req = new df.DurableHttpRequest('GET', 'https://api.github.com/users/anthonychu/events');
+    const response = yield context.df.callHttp(req);
+    return response;
+});
