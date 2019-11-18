00c8a564cd03bd64adc617245e06c4733f35b5f5
Anthony Chu <antchu@microsoft.com>
2019-11-18 09:56:27 -0800

    Add bad timer example

---
+++ BadTimer/index.js
@@ -0,0 +1,7 @@
+﻿const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const inOneMinute = new Date(new Date().getTime() + 60000);
+    yield context.df.createTimer(inOneMinute);
+    context.log('The above line should fail during replay.')
+});
